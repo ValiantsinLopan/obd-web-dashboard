cd7e2db5e7b8aab62e976e8795e50c6c20f86d46
Valentin Lopan <valiantsin_lopan@live.com>
2019-08-24 20:11:23 +0300

    Updates

---
+++ precache-manifest.49a7023701b3e4646ce9017469033fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bcee807edcc107316781c8ce8694b1",
+    "url": "/obd-web-dashboard/index.html"
+  },
+  {
+    "revision": "9413ecb54a73f978606c",
+    "url": "/obd-web-dashboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3a4d46b61078ae091cd1",
+    "url": "/obd-web-dashboard/static/js/2.8342a917.chunk.js"
+  },
+  {
+    "revision": "9413ecb54a73f978606c",
+    "url": "/obd-web-dashboard/static/js/main.d5bc8e81.chunk.js"
+  },
+  {
+    "revision": "b1b4f5190e0ba9c56309",
+    "url": "/obd-web-dashboard/static/js/runtime~main.ea6afab5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/obd-web-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
